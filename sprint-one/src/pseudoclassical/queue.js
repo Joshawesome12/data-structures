a328acc946fdca4d64f9cea800d59345062fd546
joshawesome12 <joshawesome12@yahoo.com>
2017-10-05 14:35:18 -0500

    Progress

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Queue.prototype.queue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+  
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
