76b59397bac8747132c2dd89e210f06476ea552d
joshawesome12 <joshawesome12@yahoo.com>
2017-10-06 11:09:06 -0500

    Finished linkedlist.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,35 @@
 var LinkedList = function() {
   var list = {};
+  var count = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail === null && list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    }
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    } 
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var tempHead = list.head;
+    list.head = list.head.next;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    var tf = false;
+    for (var key in list) {
+      if (list[key].value === target) {
+        tf = true;
+      }
+    }
+    return tf;
   };
 
   return list;
