b59dbeca320044c80c879fbf80c4e338d48320e7
joshawesome12 <joshawesome12@yahoo.com>
2017-10-06 18:58:09 -0500

    Progress on tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,39 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = {
+    //value: value
+    //children: [];
+  };
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //.addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+  this.children.push(Tree(value));
+  // console.log(this);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  //.contains() takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+    // tree.children[0].addChild(7);
+    console.log('this.children[i]', this.children[i]);
+    console.log('this.children[i].value', this.children[i].value);
+    if (this.children[i].children !== 0) {
+      // this.contains(this.children[i].children);
+    }
+  }
+  return false;
 };
 
 
