fa3b4d504ef30a3fe2818bf69e5e82a60d12d5be
joshuapawlik <joshawesome12@yahoo.com>
2017-10-11 09:51:43 -0500

    Finished binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,24 +9,67 @@
 //Profile this against the same operation using an array.
 
 var BinarySearchTree = function(value) {
-	var newTree = {};
+	var newTree = Object.create(BinarySearchTree.prototype);
 	newTree.left = null;
 	newTree.right = null;
+	newTree.value = value;
+	return newTree;
 };
 
 BinarySearchTree.prototype.insert = function(value){
-	var currentValue = 0;
-	if (value > currentValue){
+	var node = BinarySearchTree(value);
+	function insertNode(tree){
+
+		if (tree.value > value && tree.left === null){
+			tree.left = node;
+		}
+		else if (tree.value > value){
+			insertNode(tree.left);	
+		}
+		if (tree.value < value && tree.right === null){
+			tree.right = node;
+		}
+		else if (tree.value < value){
+			insertNode(tree.right);
+		}
 
 	}
+	insertNode(this);
+
 }
 
 BinarySearchTree.prototype.contains = function(target){
+	var tf = false;
+	function containsNode(tree){
+
+	if (tree.value === target){
+		tf = true;
+	}
+	else if (tree.value > target && tree.left !== null){
+		containsNode(tree.left);	
+	}
+	else if (tree.value < target && tree.right !== null){
+		containsNode(tree.right);
+	}
 
 }
+containsNode(this);
+return tf;
+}
 
-BinarySearchTree.prototype.depthFirstLog = function(cb){
-
+BinarySearchTree.prototype.depthFirstLog = function(callBack){
+	function cbOnTree(tree){
+	callBack.call(tree, tree.value);
+	//left
+	if (tree.left !== null){
+		cbOnTree(tree.left);
+	}
+	//right
+	if (tree.right !== null){
+		cbOnTree(tree.right);
+	}
+}
+	cbOnTree(this);
 }
 
 
